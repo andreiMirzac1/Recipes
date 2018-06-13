3672365fd101ac2b510ba1049e294fdc6e542a44
Andrei Mirzac <andrei.mirzac1@gmail.com>
2018-06-13 21:43:50 +0300

    init project

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Andrei Mirzac on 13/06/2018.
+//  Copyright © 2018 Andrei Mirzac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
